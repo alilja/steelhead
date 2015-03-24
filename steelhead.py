3e7ceecffacd9cae07ba4938c28151d3cd8d6292
Andrew Lilja <alilja@iastate.edu>
2015-03-24 14:57:03 -0500

    fixed values

--- steelhead.py
+++ steelhead.py
@@ -5,6 +5,7 @@ filename = "data.xlsx"
 COL_DATA = 0
 COL_TIME = 1
 
+print "Loading \"{0}\"".format(filename)
 workbook = xlrd.open_workbook(filename)
 try:
     sheet = workbook.sheet_by_name([s for s in workbook.sheet_names() if "reduced" in s][0])
@@ -12,8 +13,8 @@ except IndexError:
     print "Cannot find \"reduced\" worksheet."
     sys.quit()
 
-i = 0
-width = 1
+width = 5
+i = width
 maxes = []
 while i < sheet.nrows:
     i += 1
@@ -21,5 +22,8 @@ while i < sheet.nrows:
     next_row = sheet.row(i + 1)
     prev_row = sheet.row(i - 1)
 
-    if curr_row[COL_DATA] > prev_row[COL_DATA] and curr_row[COL_DATA] > next_row[COL_DATA]:
-        print curr_row
+    if curr_row[COL_DATA].value > prev_row[COL_DATA].value and curr_row[COL_DATA].value > next_row[COL_DATA].value:
+        print prev_row[COL_DATA]
+        print curr_row[COL_DATA]
+        print next_row[COL_DATA]
+        print "-"*80
